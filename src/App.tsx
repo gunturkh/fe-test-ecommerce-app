e90dd873abc4c7867013df4c087c652aa3e6c1d1
Guntur KH <guntur.kh@gmail.com>
2022-08-17 00:17:04 +0700

    feat: setup navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import "styles/global.css";
+import { Routes, Route, Link } from "react-router-dom";
+import "tailwindcss/tailwind.css";
 
 import Home from "pages";
+import CartPage from "pages/cart";
+import { Container, Footer, Header } from "components";
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Container>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+      <Footer />
+    </Container>
   );
 }
 
