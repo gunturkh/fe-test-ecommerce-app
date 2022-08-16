49c2de097f8ce1a3f995ac228f5e8dfcbd8087ad
Guntur KH <guntur.kh@gmail.com>
2022-08-16 22:07:45 +0700

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "styles/global.css";
+
+import Home from "pages";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
