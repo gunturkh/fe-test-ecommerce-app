7d7f7ebe251d1f710c6163f3a91f1b06320ad50f
Guntur KH <guntur.kh@gmail.com>
2022-08-18 18:17:16 +0700

    feat: app logo

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Logo: React.FC = () => {
-  return <img src="/icons/pankod-icon.svg" alt="react" width="96" height="58" />;
+  const navigate = useNavigate();
+  return <img src="/icons/cart.svg" alt="react" width="96" height="58"  onClick={()=> navigate('/')} className="cursor-pointer"/>;
 };
